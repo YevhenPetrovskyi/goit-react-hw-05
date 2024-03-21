657e653764ec049ec7090f632f34be152c29fdc3
YevhenPetrovskyi <71007428+YevhenPetrovskyi@users.noreply.github.com>
2024-03-21 22:07:04 +0200

    add HomePage

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import { fetchTrendingMovies } from '../../API/tmdb-api';
+
+import MoviesList from '../../components/MoviesList/MoviesList';
+import ErrorMessage from '../../components/ErrorMessage/ErrorMessage';
+import Loader from '../../components/Loader/Loader';
+
+const HomePage = () => {
+  const [loading, setLoading] = useState(false);
+  const [movies, setMovies] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const data = await fetchTrendingMovies();
+        setMovies(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <section>
+      <div className="container">
+        <h1>Trending today</h1>
+        {error && <ErrorMessage message={error} />}
+        {movies && <MoviesList movies={movies} />}
+        {loading && <Loader />}
+      </div>
+    </section>
+  );
+};
+
+export default HomePage;
